27027fa239c3dd8aa6c42e69c7fa118b4861639f
Kuye Harry <dammykuye@gmail.com>
2023-03-05 21:21:40 +0100

    attendee and event input validation

--- source/serializers/events.ts
+++ source/serializers/events.ts
@@ -1,23 +1,49 @@
-import { IsNotEmpty, IsNumber, IsString, Max, Min, IsDateString } from 'class-validator';
+import { IsNumber, IsString, IsDateString, Matches, IsOptional } from 'class-validator';
 
-export default class ProductRating {
-  @IsString()
-  @IsNotEmpty()
-  content: string;
-
-  @Min(1)
-  @Max(5)
-  @IsNumber()
-  rating: number;
-}
 
 export class AddEvent {
+  @IsString()
   name: string
+  
+  @IsString()
   description: string
+
+  @IsDateString()
   date: string
+
+  @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/, {
+    message: 'Invalid time format. Use the format HH:mm',
+  })
   startTime: string
+
+  @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/, {
+    message: 'Invalid time format. Use the format HH:mm',
+  })
   endTime: string
+
+  @IsString()
   location: string
-  totalCapacity: string
+
+  @IsNumber()
+  totalCapacity: number
 }
 
+export class AddAttendee{
+  @IsString()
+  firstname: string
+
+  @IsString()
+  email: string
+
+  @IsString()
+  lastname: string
+
+  @IsString()
+  @IsOptional()
+  contact: string
+
+  @IsString()
+  @IsOptional()
+  address: string
+
+}
