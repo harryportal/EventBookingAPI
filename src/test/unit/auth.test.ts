2addee730260d5c9b110f22e29f10515ad6d5660
Kuye Harry <dammykuye@gmail.com>
2023-03-23 12:20:50 +0100

    added unit test

---
+++ src/test/unit/auth.test.ts
@@ -0,0 +1,18 @@
+import { createJWT } from "../../utils/auth";
+import jwt from "jsonwebtoken";
+import { randomUUID } from "crypto";
+
+interface User {
+    id: string,
+    username: string
+}
+
+
+describe("Test the JWT Function", ()=>{
+    it("should return payload that matches user object", ()=>{
+        const payload: User = {username:"username", id:randomUUID()}
+        const token = createJWT(payload);
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+        expect(decode).toMatchObject(payload)
+    })
+})
